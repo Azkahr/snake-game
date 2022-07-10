bb993bd70bc50d17e74182fe460223f5af186b62
Azka <azkahar@gmail.com>
2022-07-10 13:29:14 +0700

    menjalankan fungsi - fungsi snake game, belum styling smh

--- script.js
+++ script.js
@@ -1,20 +1,82 @@
 const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d')
 
+class SnakePart {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+
 let speed = 7
 let tileCount = 20
 let tileSize = canvas.width / tileCount - 2
 let headX = 10
 let headY = 10
+const snakeParts = []
+let taillength = 6
+
+let appleX = 5
+let appleY = 5
 
 let xVelocity = 0
 let yVelocity = 0
 
+let score = 6
+
 function drawGame() {
-    clearScreen()
     changeSnakePosition()
+    let result = isGameOver()
+    if(result) return
+    
+    clearScreen()
+
+    checkAppleCollision()
+    drawApple()
     drawSnake()
-    setTimeout(drawGame, 1000/speed)
+
+    drawScore()
+    
+    setTimeout(drawGame, 1000 / speed)
+}
+
+function isGameOver() {
+    let gameOver = false
+    
+    if(yVelocity === 0 && xVelocity === 0) return false
+    
+    // walls
+    if(headX < 0) {
+        gameOver = true
+    } else if(headX === tileCount) {
+        gameOver = true 
+    } else if(headY < 0) {
+        gameOver = true
+    } else if(headY === tileCount) {
+        gameOver = true
+    }
+
+    for(let i = 0; i < snakeParts.length; i++) {
+        let part = snakeParts[i]
+        if(part.x === headX && part.y === headY) {
+            gameOver = true
+            break
+        }
+    }
+    
+    if(gameOver) {
+        ctx.fillStyle = 'white'
+        ctx.font = '50px Verdana'
+        ctx.fillText('Game Over!', canvas.width / 6.5, canvas.height /2)
+    }
+
+    return gameOver
+}
+
+function drawScore() {
+    ctx.fillStyle = 'white'
+    ctx.font = '20px Verdana'
+    ctx.fillText("Score : " + score, canvas.width-100, 20)
 }
 
 function clearScreen() {
@@ -28,6 +90,17 @@ function clearScreen() {
 }
 
 function drawSnake() {
+    ctx.fillStyle = 'green'
+    for(let i = 0; i < snakeParts.length; i++) {
+        let part = snakeParts[i]
+        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize)
+    }
+
+    snakeParts.push(new SnakePart(headX, headY)) 
+    while(snakeParts.length > taillength) {
+        snakeParts.shift()
+    }
+
     ctx.fillStyle = 'orange'
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize)
 }
@@ -37,31 +110,50 @@ function changeSnakePosition() {
     headY = headY + yVelocity
 }
 
+function drawApple() {
+    ctx.fillStyle = 'red'
+    ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize)
+}
+
+function checkAppleCollision() {
+    if(appleX == headX && appleY == headY) {
+        appleX = Math.floor(Math.random() * tileCount)
+        appleY = Math.floor(Math.random() * tileCount)
+        taillength++
+        score++
+    }
+}
+
 document.body.addEventListener('keydown', keyDown)
 
 function keyDown(e) {
     // up
     if(e.keyCode == 87) {
-        yVelocity = -1
-        xVelocity = 0
+        // agar ular tidak bisa bergerak ke arah berlawanan
+        if(yVelocity == 1) return
+            yVelocity = -1
+            xVelocity = 0
     }
 
     // down
     if(e.keyCode == 83) {
-        yVelocity = 1
-        xVelocity = 0
+        if(yVelocity == -1) return
+            yVelocity = 1
+            xVelocity = 0
     }
 
     // left
     if(e.keyCode == 65) {
-        yVelocity = 0
-        xVelocity = -1
+        if(xVelocity == 1) return
+            yVelocity = 0
+            xVelocity = -1
     }
 
     // right
     if(e.keyCode == 68) {
-        yVelocity = 0
-        xVelocity = 1
+        if(xVelocity == -1) return
+            yVelocity = 0
+            xVelocity = 1
     }
 }
 
